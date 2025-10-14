35b3660ed933d03326ab2e471fb8daa613a3cf18
akinkunminexgen <akinkunminexgen@gmail.com>
2025-10-14 14:34:32 -0300

    restructing to enable easier testing and higher maintanabilty

---
+++ CarParkManagement.DataAccess/Data/IRepository/IParkingRepository.cs
@@ -0,0 +1,23 @@
+﻿using CarParkManagement.DataAccess.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParkManagement.DataAccess.Data.IRepository
+{
+    public interface IParkingRepository
+    {
+        Task<int> GetAvailableSpaceCountAsync();
+        Task<int> GetOccupiedSpaceCountAsync();
+        Task<ParkingSpace> GetFirstAvailableSpaceAsync();
+        Task<ChargeRate?> GetChargeRateAsync(string size);
+        Task<Vehicle?> GetVehicleByRegAsync(string reg);
+        Task<ParkingAllocation> GetParkingAllocationAsync(int vehicleId);
+        Task AddVehicleAsync(Vehicle vehicle);
+        Task AddParkingAllocationAsync(ParkingAllocation parkingAllocation);
+        Task SaveChanges();
+
+    }
+}
---
+++ CarParkManagement.DataAccess/Data/IRepository/Repository/ParkingRepository.cs
@@ -0,0 +1,65 @@
+﻿using CarParkManagement.DataAccess.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParkManagement.DataAccess.Data.IRepository.Repository
+{
+    public class ParkingRepository : IParkingRepository
+    {
+        private readonly MyDbConnection _db;
+
+        public ParkingRepository( MyDbConnection db)
+        {
+            _db = db;
+        }
+        public Task AddParkingAllocationAsync(ParkingAllocation parkingAllocation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddVehicleAsync(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> GetAvailableSpaceCountAsync()
+        {
+            int v = await _db.ParkingSpaces.Where(p => !p.IsOccupied).CountAsync();
+            throw new NotImplementedException();
+        }
+
+        public Task<ChargeRate?> GetChargeRateAsync(string size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ParkingSpace> GetFirstAvailableSpaceAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetOccupiedSpaceCountAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ParkingAllocation> GetParkingAllocationAsync(int vehicleId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vehicle?> GetVehicleByRegAsync(string reg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
